635753b94dbc92ea2f6daf301a61aba7896d421d
GuttermanA <alexanderf.gutterman@gmail.com>
2018-02-02 13:46:38 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def print_status
+    if self.active
+      puts 'active'
+    else
+      puts 'inactive'
+    end
+  end
 end
